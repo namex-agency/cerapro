f8b8b6c848e1139b4111d3e4a1449a3e9d7d4cff
NamEx Agency <contact.namex@gmail.com>
2026-04-29 21:04:41 +0100

    fix: products route controller mapping

--- cerapro-backend/src/products/products.controller.ts
+++ cerapro-backend/src/products/products.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
-@Controller('products')
+@Controller('/products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  async findAll(@Query('search') search?: string) {
+  async getProducts(@Query('search') search?: string) {
     return this.productsService.getProducts(search);
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  async getProductById(@Param('id') id: string) {
     return this.productsService.getProductById(id);
   }
 }
